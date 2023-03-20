efd7d612277af3750ac54e188e3be1e7c7636f58
Claudia Miranda <clauh9@outlook.pt>
2023-03-20 17:25:57 +0000

    slider

--- main.js
+++ main.js
@@ -5,3 +5,25 @@ navToggle.addEventListener("click", () => {
   navToggle.setAttribute("aria-expanded", !primaryNav.hasAttribute("data-visible"))
   primaryNav.toggleAttribute("data-visible");
 })
+
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  adaptiveHeight: true,
+  dots: false,
+  arrows: false,
+  autoplay: false,
+  autoplaySpeed: 2500,
+  skipBtn: false,
+  responsive: {
+    768: {
+      slidesToShow: 1,
+      dots: true
+    },
+    960: {
+      // dots: false
+    },
+    1280: {
+      dots: false,
+      slidesToShow: 3,
+    }
+  }
+});
